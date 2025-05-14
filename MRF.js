36fb70da11793776a8654b1be4ffc1c8cd0159cd
divya2457 <divyaramesh2457@gmail.com>
2025-05-14 15:19:45 +0530

    MRF task

---
+++ MRF.js
@@ -0,0 +1,30 @@
+for(num=1;num<=10;num++)
+    console.log(num)//for loop
+
+let a=10
+while(a>=0){
+    console.log(a)
+    a--;
+}
+
+let i=1;
+do{
+    console.log("hai")
+    i++
+}while(i<=5)// do while
+
+let days=["sunday","monday","thuesday"]
+for(sap of days)
+{
+    console.log(sap)
+}//
+
+let obj={
+    name1:"divi",
+    age:22,
+    mark:90
+}
+for(result in obj)
+{
+    console.log(result+":"+obj[result])
+}
